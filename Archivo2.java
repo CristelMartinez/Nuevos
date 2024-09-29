3a9401c94d2f14205f710f1c4d81ff2160e98949
CristelM <martinezcristel953@gmail.com>
2024-09-29 12:00:00 -0600

    Añadir Archivo2.java en 2024-09-29T12:00:00

---
+++ Archivo2.java
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Autos</title>
+  <link rel="stylesheet" href="Autos.css">
+</head>
+<body>
+    <div class="navbar">
+        <a href="index.html">Inicio</a>
+        <a href="Autos.html">Autos</a>
+        <a href="Motos.html">Motos</a>
+    </div>
+
+<div class="main">
+  <h1>Autos</h1>
+  <ul class="cards">
+    <li class="cards_item">
+      <div class="card">
+        <div class="card_image"><img src="img/Aveo.jpg"></div>
+        <div class="card_content">
+          <h1 class="card_title">Chevrolet Aveo.</h1>
+          <p class="card_text">Precio: 299,000$</p>
+          <p class="card_text">Modelo:2020</p>
+          <button class="btn card_btn">Ir</button>
+        </div>
+      </div>
+    </li>
+    <li class="cards_item">
+      <div class="card">
+        <div class="card_image"><img src="img/Civic.jpg"></div>
+        <div class="card_content">
+          <h1 class="card_title">Honda Civic</h1>
+          <p class="card_text">Precio: 320,000$</p>
+          <p class="card_text">Modelo: 2018</p>
+          <button class="btn card_btn">Ir</button>
+        </div>
+      </div>
+    </li>
+    <li class="cards_item">
+      <div class="card">
+        <div class="card_image"><img src="img/Frontier.jpeg"></div>
+        <div class="card_content">
+          <h1 class="card_title">Nissan Frontier</h1>
+          <p class="card_text">Precio: $700,000</p>
+          <p class="card_text">Modelo: 2024</p>
+          <button class="btn card_btn">Ir</button>
+        </div>
+      </div>
+    </li>
+    <li class="cards_item">
+      <div class="card">
+        <div class="card_image"><img src="img/Tiguan.jpg"></div>
+        <div class="card_content">
+          <h1 class="card_title">Volkswaguen Tiguan</h1>
+          <p class="card_text">Precio: $349,000</p>
+          <p class="card_text">Modelo: 2018</p>
+          <button class="btn card_btn">Ir</button>
+        </div>
+      </div>
+    </li>
+    <li class="cards_item">
+      <div class="card">
+        <div class="card_image"><img src="img/Lobo.jpg"></div>
+        <div class="card_content">
+          <h1 class="card_title">Ford Lobo</h1>
+          <p class="card_text">Precio: $628,000</p>
+          <p class="card_text">Modelo: 2016</p>
+          <button class="btn card_btn">Ir</button>
+        </div>
+      </div>
+    </li>
+    <li class="cards_item">
+      <div class="card">
+        <div class="card_image"><img src="img/Mazda.jpg"></div>
+        <div class="card_content">
+          <h1 class="card_title">Mazda 3</h1>
+          <p class="card_text">Precio: $320,000</p>
+          <p class="card_text">Modelo: 2016</p>
+          <button class="btn card_btn">Ir</button>
+        </div>
+      </div>
+    </li>
+    <li class="cards_item">
+      <div class="card">
+        <div class="card_image"><img src="img/Vento.jpg"></div>
+        <div class="card_content">
+          <h1 class="card_title">Volkswaguen Vento</h1>
+          <p class="card_text">Precio: $219,000</p>
+          <p class="card_text">Modelo: 2017</p>
+          <button class="btn card_btn">Ir</button>
+        </div>
+      </div>
+    </li>
+    <li class="cards_item">
+      <div class="card">
+        <div class="card_image"><img src="img/Versa.jpg"></div>
+        <div class="card_content">
+          <h1 class="card_title">Nissan Versa</h1>
+          <p class="card_text">Precio: $185,000</p>
+          <p class="card_text">Modelo: 2016</p>
+          <button class="btn card_btn">Ir</button>
+        </div>
+      </div>
+    </li>
+    <li class="cards_item">
+      <div class="card">
+        <div class="card_image"><img src="img/March.png"></div>
+        <div class="card_content">
+          <h1 class="card_title">Nissan March</h1>
+          <p class="card_text">Precio: $272,000</p>
+          <p class="card_text">Modelo: 2024</p>
+          <button class="btn card_btn">Ir</button>
+        </div>
+      </div>
+    </li>
+  </ul>
+</div>
+</body>
+</html>
