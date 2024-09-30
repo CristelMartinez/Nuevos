c85cd35812283e0888c8b829b9a8d5e8d1fc3d44
CristelM <martinezcristel953@gmail.com>
2024-09-30 12:00:00 -0600

    Añadir Archivo3.java en 2024-09-30T12:00:00

---
+++ Archivo3.java
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Motos</title>
+  <link rel="stylesheet" href="Autos.css">
+</head>
+<body>
+    <div class="navbar">
+        <a href="index.html">Inicio</a>
+        <a href="Autos.html">Autos</a>
+        <a href="Motos.html">Motos</a>
+    </div>
+
+<div class="main">
+  <h1>Motos</h1>
+  <ul class="cards">
+    <li class="cards_item">
+      <div class="card">
+        <div class="card_image"><img src="img/Harley.jpg"></div>
+        <div class="card_content">
+          <h1 class="card_title">Harley Davidson.</h1>
+          <p class="card_text">Precio: 30,600$</p>
+          <p class="card_text">Modelo: 2015</p>
+          <button class="btn card_btn">Ir</button>
+        </div>
+      </div>
+    </li>
+    <li class="cards_item">
+      <div class="card">
+        <div class="card_image"><img src="img/Indian.jpg"></div>
+        <div class="card_content">
+          <h1 class="card_title">Indian Scout</h1>
+          <p class="card_text">Precio: 200,000$</p>
+          <p class="card_text">Modelo: 2020</p>
+          <button class="btn card_btn">Ir</button>
+        </div>
+      </div>
+    </li>
+    <li class="cards_item">
+      <div class="card">
+        <div class="card_image"><img src="img/Pulsar.jpg"></div>
+        <div class="card_content">
+          <h1 class="card_title">Pulsar</h1>
+          <p class="card_text">Precio: $30,000</p>
+          <p class="card_text">Modelo: 2017</p>
+          <button class="btn card_btn">Ir</button>
+        </div>
+      </div>
+    </li>
+    <li class="cards_item">
+      <div class="card">
+        <div class="card_image"><img src="img/Italika.jpg"></div>
+        <div class="card_content">
+          <h1 class="card_title">Italika RT</h1>
+          <p class="card_text">Precio: $32,000</p>
+          <p class="card_text">Modelo: 2020</p>
+          <button class="btn card_btn">Ir</button>
+        </div>
+      </div>
+    </li>
+    <li class="cards_item">
+      <div class="card">
+        <div class="card_image"><img src="img/Pulsar2.jpg"></div>
+        <div class="card_content">
+          <h1 class="card_title">Bajaj Pulsar</h1>
+          <p class="card_text">Precio: $60,000</p>
+          <p class="card_text">Modelo: 2022</p>
+          <button class="btn card_btn">Ir</button>
+        </div>
+      </div>
+    </li>
+    <li class="cards_item">
+      <div class="card">
+        <div class="card_image"><img src="img/R6.jpeg"></div>
+        <div class="card_content">
+          <h1 class="card_title">Yamaha R6</h1>
+          <p class="card_text">Precio: $319,000</p>
+          <p class="card_text">Modelo: 2020</p>
+          <button class="btn card_btn">Ir</button>
+        </div>
+      </div>
+    </li>
+  </ul>
+</div>
+
+</body>
+</html>
