baed3c567f114d94b87de6745d5d1fe7a68ce1d5
CristelM <martinezcristel953@gmail.com>
2024-10-01 12:00:00 -0600

    Añadir Archivo4.java en 2024-10-01T12:00:00

---
+++ Archivo4.java
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="Pagina1.css">
+</head>
+
+<body>
+    <div class="navbar">
+        <a href="#">Inicio</a>
+        <a href="Autos.html">Autos</a>
+        <a href="Motos.html">Motos</a>
+    </div>
+
+    <div class="container">
+        <div class="content">
+            <h1 class="title">Bienvenido</h1>
+            <p class="text"><br>
+                <p>Descubre nuestra exclusiva colección de vehículos.</p>
+                <p>En nuestra concesionaria, encontraras de todo tipo de vehiculos, desde autos y motocicletas usados hasta la version mas reciente de tu modelo favorito!</p>
+        </div>
+       
+    </div>
+</body>
+
+</html>
